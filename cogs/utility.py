8c912c10c1962dfdc4905e0be7dea104a058a68f
wolfgamerpro <wolfireyt@gmail.com>
2020-09-05 23:54:05 -0300

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -88,7 +88,7 @@ class ModmailHelpCommand(commands.HelpCommand):
         bot = self.context.bot
 
         # always come first
-        default_cogs = [bot.get_cog("Modmail"), bot.get_cog("Utility"), bot.get_cog("Plugins")]
+        default_cogs = [bot.get_cog("Modmail"), bot.get_cog("Utility")]
 
         default_cogs.extend(c for c in cogs if c not in default_cogs)
 
